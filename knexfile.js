9b2c946810f7ea8ee9f998a61d31efe31cd5e8dc
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-12 22:44:39 -0500

    .

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 
 module.exports = {
-  client: "pg",
-  debug: true,
-  connection: process.env.DATABASE_URL,
-  migrations: {
-    directory: "./data/migrations"
-  },
-  ssl: true
+  production: {
+    client: "pg",
+    debug: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    ssl: true
+  }
 };
