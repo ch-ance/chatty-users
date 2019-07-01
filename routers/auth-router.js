3c20233ed06049504a211b3513c95fa612f06370
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-01 12:29:21 -0500

    returns a jwt

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -1,33 +1,64 @@
-const authRouter = require("express").Router();
-const Users = require("../data/helpers/users-model");
+require('dotenv').config()
 
-authRouter.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
+const authRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const Users = require('../data/helpers/users-model')
+const secret = process.env.JWT_SECRET || 'secret'
 
-    const userID = Math.floor(Math.random() * 1000);
+authRouter.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body
 
-    const user = {
-      username,
-      password,
-      userID
-    };
-    await Users.add(user);
-    res.status(201).json({ user });
-  } catch (error) {
-    res.status(400).json({ message: "Error adding user" });
-  }
-});
+        const userID = Math.floor(Math.random() * 1000)
 
-authRouter.post("/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
+        const user = {
+            username,
+            password,
+            userID
+        }
+        await Users.add(user)
+        res.status(201).json({ user })
+    } catch (error) {
+        res.status(400).json({ message: 'Error adding user' })
+    }
+})
 
-    const [user] = await Users.findByUsername(username);
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "User not found" });
-  }
-});
+authRouter.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body
 
-module.exports = authRouter;
+        const [user] = await Users.findByUsername(username)
+
+        const hash = bcrypt.hashSync(user.password, 10)
+
+        if (bcrypt.compareSync(password, hash)) {
+            const token = genToken(user)
+            res.status(201).json(
+                {
+                    username: user.username,
+                    password: hash
+                },
+                token
+            )
+        } else {
+            res.status(500).json({ message: 'Invalid credentials ' })
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'User not found' })
+    }
+})
+
+function genToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username
+    }
+
+    const options = {
+        expiresIn: '1d'
+    }
+    return jwt.sign(payload, secret, options)
+}
+
+module.exports = authRouter
