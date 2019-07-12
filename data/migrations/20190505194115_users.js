5832ae3614a31989a70fc58cdf51f954f297aec9
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-12 09:56:00 -0500

    modify migrations

--- data/migrations/20190505194115_users.js
+++ data/migrations/20190505194115_users.js
@@ -1,19 +1,19 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("users", users => {
-    users.increments("ID");
-    users
-      .integer("userID")
-      .notNullable()
-      .unique();
-    users
-      .string("username", 16)
-      .unique()
-      .notNullable();
+    return knex.schema.createTable('users', users => {
+        users.increments('ID')
+        users
+            .string('userID')
+            .notNullable()
+            .unique()
+        users
+            .string('username', 16)
+            .unique()
+            .notNullable()
 
-    users.string("password", 128).notNullable();
-  });
-};
+        users.string('password', 128).notNullable()
+    })
+}
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("users");
-};
+    return knex.schema.dropTableIfExists('users')
+}
