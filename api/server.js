592e82a1c18a831de39d69602ee12ced8c5e204f
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-16 21:07:56 -0500

    websocket server is listening on a different port at the same time as the db server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const helmet = require('helmet')
+const http = require('http')
 const cors = require('cors')
 const morgan = require('morgan')
 const usersRouter = require('../routers/userRouter.js')
 const authRouter = require('../routers/auth-router')
 const server = express()
+
+const wsApp = express()
+
+wsApp.use(express.static(__dirname + '/'))
+
+const wsServer = http.createServer(wsApp)
 
 server.use(helmet())
 server.use(cors())
@@ -18,4 +25,7 @@ server.get('/', (req, res) => {
 server.use('/api/users', usersRouter)
 server.use('/api/auth', authRouter)
 
-module.exports = server
+module.exports = {
+    server,
+    wsServer,
+}
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-require("dotenv").config();
-const server = require("./api/server.js");
+require('dotenv').config()
+const WebSocket = require('ws').Server
 
-const port = process.env.PORT || 5432;
+const { server, wsServer } = require('./api/server.js')
+const port = process.env.PORT || 5432
+
+const wsPORT = 2020
 
 server.listen(port, function() {
-  console.log(`*** Server listening on port ${port}. ***`);
-});
+    console.log(`*** Server listening on port ${port}. ***`)
+})
+
+wsServer.listen(wsPORT, () => {
+    console.log('web socket db is listenin')
+})
